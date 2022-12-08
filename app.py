31dba7312f936db08b46147ea70359366d70da30
mxrtydluffy <marty.marinda@students.dominican.edu>
2022-12-07 22:38:00 -0800

    did TODOs

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, redirect, render_template, url_for
-from flask_pymongo import PyMongo
+# from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from pymongo import MongoClient
 
 ############################################################
 # SETUP
@@ -8,8 +9,12 @@ from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
-app.config["MONGO_URI"] = "mongodb://localhost:27017/plantsDatabase"
-mongo = PyMongo(app)
+# app.config["MONGO_URI"] = "mongodb://localhost:27017/plantsDatabase"
+# mongo = PyMongo(app)
+
+client = MongoClient("localhost", 27017)
+plants = client.plants_database.plant
+harvest = client.plants_database.harveests
 
 ############################################################
 # ROUTES
@@ -21,7 +26,7 @@ def plants_list():
 
     # TODO: Replace the following line with a database call to retrieve *all*
     # plants from the Mongo database's `plants` collection.
-    plants_data = ''
+    plants_data = plants.find()
 
     context = {
         'plants': plants_data,
@@ -40,10 +45,10 @@ def create():
         # TODO: Get the new plant's name, variety, photo, & date planted, and 
         # store them in the object below.
         new_plant = {
-            'name': '',
-            'variety': '',
-            'photo_url': '',
-            'date_planted': ''
+            'name': request.form["plant_name"],
+            'variety': request.form["variety"],
+            'photo_url': request.form["photo"],
+            'date_planted': request.form["date_planted"]
         }
         # TODO: Make an `insert_one` database call to insert the object into the
         # database's `plants` collection, and get its inserted id. Pass the 
